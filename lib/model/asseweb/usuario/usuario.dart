17f71b0ec6350448343e175d38518e03805f49bd
Wesley Ziroldo Franquim <66917610+Ziroldo@users.noreply.github.com>
2022-06-28 14:43:42 -0300

    Model Usuário Asseweb

--- lib/model/asseweb/usuario/usuario.dart
+++ lib/model/asseweb/usuario/usuario.dart
@@ -1,55 +1,118 @@
 
 
+import 'dart:ffi';
+
+import 'package:assecontservices/assecontservices.dart';
+import 'package:flutter_awesome_alert_box/flutter_awesome_alert_box.dart';
+
 class UsuarioAsseweb {
   int? id;
-  String? nome;
-  String? empresa;
-  String? cnpj;
-  String? registro;
-  String? cpf;
+  String? name;
   String? email;
+  String? phoneNumber;
   String? ddd;
-  String? celular;
+  String? password;
+  DateTime? lastAcess;
+  DateTime? birthday;
+  String? version;
+  DateTime? resignation;
+  String? uid;
+  Bool? master;
+  List<Companies>? companies;
 
-  UsuarioAsseweb({this.id, this.nome, this.empresa, this.email, this.cnpj, this.cpf, this.registro, this.ddd, this.celular});
+  UsuarioAsseweb({this.id, this.name, this.email, this.phoneNumber, this.ddd, this.password, this.lastAcess, this.birthday, this.version,
+    this.resignation, this.uid, this.master, this.companies});
 
-  UsuarioAsseweb copyWith({
-    String? email, String? nome, String? empresa, String? cpf, String? cnpj, String? registro, String? celular
+    UsuarioAsseweb copyWith({
+
+    int? id, String? name, String? email, String? phoneNumber, String? ddd, String? password, DateTime? lastAcess, DateTime? birthday,
+      String? version, DateTime? resignation, String? uid, Bool? master, List<Companies>? companies
+
   }) => UsuarioAsseweb(
-      id: id ?? this.id,
-      nome: nome ?? this.nome,
-      empresa: empresa ?? this.empresa,
-      email: email ?? this.email,
-      cpf: cpf ?? this.cpf,
-      cnpj: cnpj ?? this.cnpj,
-      registro: registro ?? this.registro,
-      ddd: ddd ?? this.ddd,
-      celular: celular ?? this.celular
+
+                id: id ?? this.id,
+                name: name ?? this.name,
+                email: email ?? this.email,
+                phoneNumber: phoneNumber ?? this.phoneNumber ,
+                ddd: ddd ?? this.ddd ,
+                password: password ?? this.password,
+                lastAcess: lastAcess ?? this.lastAcess,
+                birthday: birthday ?? this.birthday,
+                version: version ?? this.version,
+                resignation: resignation ?? this.resignation,
+                uid: uid ?? this.uid,
+                master: master ?? this.master,
+                companies: companies ?? this.companies
+
+
   );
+
+
 
   UsuarioAsseweb.fromMap(Map map){
     this.id =   map["id"] == null ? null : map["id"];
+    this.name = map["name"] == null ? null : map["name"];
     this.email = map["email"] == null ? null : map["email"];
-    this.empresa = map["empresa"] == null ? null : map["empresa"];
-    this.nome = map["nome"] == null ? null : map["nome"];
-    this.cpf = map["cpf"] == null ? null : map["cpf"];
-    this.cnpj = map["cnpj"] == null ? null : map["cnpj"];
-    this.registro = map["registro"] == null ? null : map["registro"];
+    this.phoneNumber = map["phoneNumber"] == null ? null : map["phoneNumber"];
     this.ddd = map["ddd"] == null ? null : map["ddd"];
-    this.celular = map["celular"] == null ? null : map["celular"];
+    this.password = map["password"] == null ? null : map["password"];
+    this.lastAcess = map["lastAcess"] == null ? null : map["lastAcess"];
+    this.birthday = map["birthday"] == null ? null : map["birthday"];
+    this.version = map["version"] == null ? null : map["version"];
+    this.resignation = map["resignation"] == null ? null : map["resignation"];
+    this.uid = map["uid"] == null ? null : map["uid"];
+    this.master = map["master"] == null ? null : map["master"];
+    this.companies = map["companies"] == null ? null : map["companies"].map((e) => Companies.fromMap(e)).toList();
+
   }
 
   Map<String, dynamic> toMap(){
     return {
       'id': id,
+      'name': name,
       'email': email,
-      'empresa': empresa,
-      'nome': nome,
-      'cpf': cpf,
-      'cnpj': cnpj,
-      'registro': registro,
+      'phoneNumber': phoneNumber,
       'ddd': ddd,
-      'celular': celular,
+      'password': password,
+      'lastAcess': lastAcess,
+      'birthday': birthday,
+      'version': version,
+      'resignation': resignation,
+      'uid': uid,
+      'master': master,
+      'companies': companies,
+
     };
+  }
+}
+
+class Companies{
+  int? id;
+  String? number;
+  String? name;
+  String? fantasyName;
+  String? cnpj;
+
+  Companies({this.id, this.number, this.name, this.fantasyName, this.cnpj});
+
+  Companies copyWith({
+    int? id, String? number, String? name, String? fantasyName, String? cnpj
+  }) => Companies(
+
+        id: id ?? this.id,
+        number: number ?? this.number,
+        name: name ?? this.name,
+        fantasyName: fantasyName ?? this.fantasyName,
+        cnpj: cnpj ?? cnpj ?? this.cnpj,
+  );
+
+  Companies.fromMap(Map map){
+
+    this.id = map["id"] == null ? null : map["id"];
+    this.number = map["number"] == null ? null : map["number"];
+    this.name = map["name"] == null ? null : map["name"];
+    this.fantasyName = map["fantasyName"] == null ? null : map["fantasyName"];
+    this.cnpj = map["cnpj"] == null ? null : map["cnpj"];
+
   }
 }
