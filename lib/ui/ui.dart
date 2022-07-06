89764fa5824774d0e11d512dbb81a211dab78d7c
Gabriel Mattos <43770191+pretolio@users.noreply.github.com>
2022-07-05 23:43:51 -0300

    Update ui.dart

--- lib/ui/ui.dart
+++ lib/ui/ui.dart
@@ -1,3 +1,5 @@
 export 'smartphone/intro/intro_screen.dart';
 export 'smartphone/tela_base/custom_menu_item.dart';
 export 'smartphone/tela_base/custom_scaffold.dart';
+export 'smartphone/holerite/holerite_screen.dart';
+export 'smartphone/config/config_screen.dart';
