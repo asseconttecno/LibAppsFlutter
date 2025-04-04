48f62e3d36303afda388d5cb3af89baac13d3ca0
Gabriel Mattos <43770191+pretolio@users.noreply.github.com>
2025-04-04 16:59:24 -0300

    Update custom_snackbar.dart

--- lib/common/custom_snackbar.dart
+++ lib/common/custom_snackbar.dart
@@ -90,26 +90,34 @@ class CustomSnackbar {
   }
 
   static home(String text, Color cor){
-    ScaffoldMessenger.of(Config.scaffoldKey.currentContext!).showSnackBar(SnackBar(
-      backgroundColor: cor,
-      duration: const Duration(seconds: 3),
-      content: CustomText.text(text, style: const TextStyle(color: Colors.white),),
-    ));
+    if(Config.scaffoldKey.currentContext != null && Config.scaffoldKey.currentContext!.mounted){
+      ScaffoldMessenger.of(Config.scaffoldKey.currentContext!).showSnackBar(SnackBar(
+        backgroundColor: cor,
+        duration: const Duration(seconds: 3),
+        content: CustomText.text(text, style: const TextStyle(color: Colors.white),),
+      ));
+    }
   }
 
   static scaffoldKey(GlobalKey<ScaffoldState> scaffoldKey, String text, Color cor, ){
-    ScaffoldMessenger.of(scaffoldKey.currentContext!).showSnackBar(SnackBar(
-      backgroundColor: cor,
-      duration: const Duration(seconds: 3),
-      content: CustomText.text(text, style: const TextStyle(color: Colors.white),),
-    ));
+    if(scaffoldKey.currentContext != null && scaffoldKey.currentContext!.mounted){
+      ScaffoldMessenger.of(scaffoldKey.currentContext!).showSnackBar(SnackBar(
+        backgroundColor: cor,
+        duration: const Duration(seconds: 3),
+        content: CustomText.text(text, style: const TextStyle(color: Colors.white),),
+      ));
+    }
+
   }
 
   static context(BuildContext context, String text, Color cor){
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      backgroundColor: cor,
-      duration: const Duration(seconds: 3),
-      content: CustomText.text(text, style: const TextStyle(color: Colors.white),),
-    ));
+    if(context.mounted){
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        backgroundColor: cor,
+        duration: const Duration(seconds: 3),
+        content: CustomText.text(text, style: const TextStyle(color: Colors.white),),
+      ));
+    }
+
   }
 }
